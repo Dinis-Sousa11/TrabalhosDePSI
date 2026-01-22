c25399c87439e0c101a0896c06edac25592d7af9
Dinis-Sousa11 <sousadinis11@gmail.com>
2026-01-22 15:23:45 +0000

    Código final projeto1 mod4

--- projeto 1 mod4.py
+++ projeto 1 mod4.py
@@ -1,41 +1,56 @@
 import random
 
-print("=== Gerador De Passwords ===")
-print("1 - Iniciais das palavras + números")
-print("2 - Iniciais + números + símbolo")
-print("3 - Palavra inteira + números")
+while True:
+    print("\n=== Gerador De Passwords ===")
+    print("1 - Iniciais das palavras + números")
+    print("2 - Iniciais + números + símbolo")
+    print("3 - Palavra inteira + números")
+    print("0 - Sair")
 
-opcao = input("Escolha uma opção: ")
 
-frase = input("Digite uma frase: ")
-palavras = frase.split()
-senha = ""
+    opcao = input("Escolha uma opção: ")
 
-#iniciais+numeros
-if opcao == "1":
-    for palavra in palavras:
-        senha += palavra[0].upper()
-    senha += str(random.randint(0, 999))
+    # Opção para sair do programa
+    if opcao == "0":
+        print("Adeus!!👋")
+        break
 
-#palavra+numeros+simbolos
-elif opcao == "2":
-    for palavra in palavras:
-        senha += palavra[0].upper()
-    senha += str(random.randint(0, 999))
-    senha += random.choice("!@#$%&§*")
 
-#palavra+numeros
-elif opcao == "3":
-    senha = random.choice(palavras).capitalize()
-    senha += str(random.randint(100, 9999))
+    frase = input("Digite uma frase: ")
 
-else:
-    print("Opção inválida!")
-    exit()
+    #divide a frase em palavras (lista)
+    palavras = frase.split()
 
-# Embaralhar a senha
-lista_senha = list(senha)
-random.shuffle(lista_senha)
-senha = "".join(lista_senha)
+    #onde a senha vai ser feita
+    senha = ""
 
-print("Senha gerada:", senha)
+    #1 iniciais das palavras+numeros
+    if opcao == "1":
+        for palavra in palavras:
+            senha += palavra[0].upper()  #primeira letra de cada palavra em maiuscula
+        senha += str(random.randint(0, 999))  #adiciona um número aleatorio
+
+    #2 iniciais+numeros+simbolo
+    elif opcao == "2":
+        for palavra in palavras:
+            senha += palavra[0].upper()
+        senha += str(random.randint(0, 999))
+        senha += random.choice("!@#$%&§*")  # adiciona um simbolo aleatorio
+
+    #3 palavra inteira aleatoria+numeros
+    elif opcao == "3":
+        senha = random.choice(palavras).capitalize()  #escolhe uma palavra aleatoria
+        senha += str(random.randint(100, 9999))
+
+
+    else:
+        print("Opção inválida!")
+        continue  #volta ao inicio do menu
+
+    #embaralha os caracteres da senha
+    lista_senha = list(senha)     # Converte a senha numa lista
+    random.shuffle(lista_senha)   # Embaralha a lista
+    senha = "".join(lista_senha)  # Converte novamente para string
+
+    #senha final
+    print("Senha gerada:", senha)
